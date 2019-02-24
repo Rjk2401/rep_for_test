5b1a71637cea195da431a55e9670b200150428c1
Rjk2401 <rjk2401@mail.ru>
2019-02-24 16:07:44 +0700

    qqqqqqqqq

--- src/hello.c
+++ src/hello.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World33334!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World123!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
